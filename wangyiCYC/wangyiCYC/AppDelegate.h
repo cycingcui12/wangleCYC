50d969c8a3e853e6f4070b6fb6df0af4c601dd8b
cyc <cyc@cycdemacbook-pro.local>
2016-01-12 08:59:42 +0800

    新建

---
+++ wangyiCYC/wangyiCYC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wangyiCYC
+//
+//  Created by cyc on 16/1/12.
+//  Copyright © 2016年 cyc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wangyiCYC/wangyiCYC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wangyiCYC
+//
+//  Created by cyc on 16/1/12.
+//  Copyright © 2016年 cyc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
